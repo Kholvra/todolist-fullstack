ab9e2615f6a91611de410274feb2ef11f2652441
kolvra <sabrisirajkholbiabby@gmail.com>
2025-11-02 12:19:08 +0700

    feat: check if not have session, go to page login

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,13 @@
 import Greet from "@/components/home-component/greet"
+import { auth } from "@/server/auth"
+import { redirect } from "next/navigation"
 
-export default function Home(){
-    
+
+export default async function Home(){
+    const session = await auth()
+    if(!session){
+        redirect('/login')
+    }
     return <div>
         <Greet/>
     </div>
