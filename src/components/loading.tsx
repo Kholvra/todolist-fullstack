9d65681adffbbc87942d902946f77f73f0a93a72
kolvra <sabrisirajkholbiabby@gmail.com>
2025-11-04 00:13:51 +0700

    style: change color to neutral

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,16 +1,14 @@
 export default function LoadingScreen() {
   return (
-    <div className="flex items-center justify-center w-full bg-white">
+    <div className="flex items-center justify-center w-full bg-white select-none">
       <div className="flex flex-col items-center gap-4">
-        {/* Spinner */}
         <div className="relative w-12 h-12">
           <div
-            className="w-full h-full border-2 border-transparent border-t-slate-800 border-r-slate-800 rounded-full animate-spin"
+            className="w-full h-full border-2 border-transparent border-t-neutral-800 border-r-neutral-800  rounded-full animate-spin"
             aria-label="Loading"
           />
         </div>
-        {/* Loading text */}
-        <p className="text-slate-800 text-sm font-medium tracking-wide">Loading...</p>
+        <p className="text-neutral-800 text-sm font-medium tracking-wide">Loading...</p>
       </div>
     </div>
   )
