492724ce3ba78ea2d6bdfdd3e0e8965c47536504
kolvra <sabrisirajkholbiabby@gmail.com>
2025-11-03 08:41:18 +0700

    fix: render glitch on login page to verify

--- src/app/(main)/login/page.tsx
+++ src/app/(main)/login/page.tsx
@@ -1,57 +1,76 @@
-'use client'
+"use client";
 
-import { useState, type FormEvent } from "react"
-import { signIn, useSession, signOut } from "next-auth/react"
-import { useRouter } from "next/navigation"
+import { useEffect, useState, type FormEvent } from "react";
+import { signIn, useSession, signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
-export default function Login(){
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
 
-    const [email, setEmail] = useState('')
-    const {data: session,status} = useSession()
-    const router = useRouter()
-    const [isLoading,setIsLoading] = useState(false)
+  useEffect(() => {
+    setIsLoading(false);
+  }, []); //prevent component render before router push to verify after submit form.
 
-    const loginSubmitHandler=async (e:FormEvent<HTMLFormElement>)=>{
-        e.preventDefault();
-        if (!session){
-            setIsLoading(true)
-            const result = await signIn('nodemailer',{email, redirect:false, redirectTo:'/'})
-            const encodedEmail = encodeURIComponent(email)
-            if (result?.ok){
-                router.push(`/verify?email=${encodedEmail}`)
-            } else{
-                alert('failed sending magic link')
-            }
-            
-            setTimeout(()=>{
-                setIsLoading(false)
-            },200)
-
-
-            
-        } else{
-            await signOut();
-        }
+  const loginSubmitHandler = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!session) {
+      setIsLoading(true);
+      const result = await signIn("nodemailer", {
+        email,
+        redirect: false,
+        redirectTo: "/",
+      });
+      const encodedEmail = encodeURIComponent(email);
+      if (result?.ok) {
+        router.push(`/verify?email=${encodedEmail}`);
+      } else {
+        alert("failed sending magic link");
+      }
+    } else {
+      await signOut();
     }
-    
+  };
 
-    if (status==='loading'){
-        return <span>loading</span>
-    }
+  if (status === "loading") {
+    return <span>loading</span>;
+  }
 
-    if (isLoading){
-        return <span>loading</span>
-    }
+  if (isLoading) {
+    return <span>loading</span>;
+  }
 
-    return(
-        <div className="flex flex-col size-full justify-center items-center">
-            <h1 className="text-4xl font-bold mb-2">Sign In</h1>
-            <span className="text-neutral mb-7">Use your email to continue</span>
-            <form action="" className=" flex flex-col w-100 justify-center gap-3" onSubmit={(e)=>loginSubmitHandler(e)}>
-                {!session?<input type="text" className="p-1 py-2 border border-neutral rounded-lg focus:outline-none focus:ring-3 focus:ring-neutral-400/50 transition duration-500 ease-in-out shadow-xl" value={email} onChange={(e)=>setEmail(e.target.value)}/>:<span>You already Sign In</span>}
-                <button type="submit" className={`rounded-lg p-2 py-2 bg-black ${email.length>0?'opacity-100':'opacity-50'} text-white text-lg transition duration-300`}>{!session? 'Sign In':'Sign Out'}</button>
-            </form>
-            <span className="text-neutral mt-7">We&apos;ll send a login link to your email</span>
-        </div>
-    )
+  return (
+    <div className="flex size-full flex-col items-center justify-center">
+      <h1 className="mb-2 text-4xl font-bold">Sign In</h1>
+      <span className="text-neutral mb-7">Use your email to continue</span>
+      <form
+        action=""
+        className="flex w-100 flex-col justify-center gap-3"
+        onSubmit={(e) => loginSubmitHandler(e)}
+      >
+        {!session ? (
+          <input
+            type="text"
+            className="border-neutral rounded-lg border p-1 py-2 shadow-xl transition duration-500 ease-in-out focus:ring-3 focus:ring-neutral-400/50 focus:outline-none"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        ) : (
+          <span>You already Sign In</span>
+        )}
+        <button
+          type="submit"
+          className={`rounded-lg bg-black p-2 py-2 ${email.length > 0 ? "opacity-100" : "opacity-50"} text-lg text-white transition duration-300`}
+        >
+          {!session ? "Sign In" : "Sign Out"}
+        </button>
+      </form>
+      <span className="text-neutral mt-7">
+        We&apos;ll send a login link to your email
+      </span>
+    </div>
+  );
 }
