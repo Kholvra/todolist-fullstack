bc6e2ac555b0d3f3359fc4d25264fcf958fb2e7c
kolvra <sabrisirajkholbiabby@gmail.com>
2025-11-04 11:44:26 +0700

    style: no overflow auto on todos

--- src/components/page-wrapper.tsx
+++ src/components/page-wrapper.tsx
@@ -5,10 +5,8 @@ interface PageWrapperProps {
 }
 export default function PageWrapper({ children }: PageWrapperProps) {
   return (
-    <div className=" h-screen overflow-hidden">
-      <div className="mx-auto md:h-full mt-10 md:mt-0 p-5 lg:w-1/3">
+    <div className="min-h-screen mx-auto p-5 lg:w-1/3">
         {children}
-      </div>
     </div>
   );
 }
--- src/components/todos.tsx
+++ src/components/todos.tsx
@@ -3,20 +3,13 @@
 import { api } from "@/trpc/react";
 import CreateTodo from "./create-todo";
 import Todo from "./todo";
-import { useEffect, type ChangeEvent, type FormEvent } from "react";
+import { type ChangeEvent, type FormEvent } from "react";
 import { todoInput } from "@/types/todo-type";
 import LoadingScreen from "./loading";
 
 export default function Todos() {
   const { data: todos, isLoading, isError } = api.todo.all.useQuery();
     const trpc = api.useUtils();
-  
-  useEffect(() => {
-    document.body.classList.add("overflow-hidden");
-    return () => {
-      document.body.classList.remove("overflow-hidden");
-    };
-  }, []);
 
     const { mutate } = api.todo.create.useMutation({
     onSettled: async () => {
@@ -112,7 +105,7 @@ export default function Todos() {
   return (
     <div>
       <CreateTodo handler={createTodo} />
-      <div className="mt-10 mb-5 flex flex-row items-center justify-between overflow-hidden">
+      <div className="mt-10 mb-5 flex flex-row items-center justify-between">
         {todos?.length?<span className="text-lgpublic font-bold">{`My Tasks (${listNotDoneTodos?.length})`}</span>:''}
         {listDoneTodos?.length ? (
           <span className="text-neutral text-sm md:text-base">{`${listDoneTodos?.length} completed`}</span>
@@ -120,7 +113,7 @@ export default function Todos() {
           ""
         )}
       </div>
-      <div className="max-h-[calc(100vh-350px)] lg:max-h-[calc(100vh-300px)] overflow-y-auto">
+      <div>
         {todos?.length ? (
           <ul>
             {todos.map((todo) => {
