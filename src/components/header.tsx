7f7ababf869cd877415e7bed5ecd53d9796daee8
kolvra <sabrisirajkholbiabby@gmail.com>
2025-11-20 14:59:42 +0700

    style: no more Sign Out text

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,19 +1,22 @@
 import { PiSignOutBold } from "react-icons/pi";
 import SignOutButton from "./sign-out-button";
 
-
-interface HeaderProps{
-    email: string|null|undefined
+interface HeaderProps {
+  email: string | null | undefined;
 }
 
-export default function Header({email}:HeaderProps){
-    return(
-        <header className="flex flex-row justify-between items-center w-full mb-10">
-            <div>
-                <h1 className="text-xl md:text-2xl font-bold mb-1">Todo List</h1>
-                <span className="text-neutral">{email}</span>
-            </div>
-            <div><SignOutButton className="flex flex-row items-center gap-2 p-2 bg-neutral-400/10 md:bg-transparent hover:lg:bg-neutral-400/10 rounded-lg transition cursor-pointer"><PiSignOutBold className="size-5"/><span className="hidden md:block font-semibold">Sign Out</span></SignOutButton></div>
-        </header>
-    )
+export default function Header({ email }: HeaderProps) {
+  return (
+    <header className="mb-10 flex w-full flex-row items-center justify-between">
+      <div>
+        <h1 className="mb-1 text-xl font-bold md:text-2xl">Todo List</h1>
+        <span className="text-neutral">{email}</span>
+      </div>
+      <div>
+        <SignOutButton className="flex cursor-pointer flex-row items-center gap-2 rounded-lg bg-neutral-400/10 p-2 transition md:bg-transparent hover:lg:bg-neutral-400/10">
+          <PiSignOutBold className="size-5" />
+        </SignOutButton>
+      </div>
+    </header>
+  );
 }
